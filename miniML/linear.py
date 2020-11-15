99160897a3c34904765be8ea1ac46cc0d3a7a90e
Yonas Geb <yonasgebeyaw1990@gmail.com>
2020-11-16 00:28:20 +0300

    __cost for logistic reg and gradient optimizer demo

--- miniML/linear.py
+++ miniML/linear.py
@@ -11,13 +11,13 @@ class LinearRegression:
     # def fit()
     # def predict()
 
-    def cost(self, X, y, theta):
+    def __cost(self, X, y, theta):
         m, n = self.X.shape
         h = X.dot(theta)
 
         J = 1/(2*m)*np.sum((h - y)**2)
 
-        grad = X.T.dot(X.dot(theta) - y)
+        grad = X.T.dot(h - y)
 
         return J, grad
 
@@ -28,3 +28,24 @@ class LogisticRegression:
     '''
     # def fit()
     # def predict()
+
+    def sigmoid(self, z):
+        '''
+        A sigmoid function is a mathematical function having a characteristic 
+        "S"-shaped curve or sigmoid curve. 
+
+        1.0 / (1.0 + np.exp(-z))
+        '''
+        return 1.0 / (1.0 + np.exp(-z))
+
+    def __cost(self, X, y, theta):
+        m, n = X.shape
+
+        h = self.sigmoid(X.dot(theta))
+
+        J = 1/m*(-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
+
+        # include 1/m?
+        grad = X.T.dot(h-y)
+
+        return J, grad
